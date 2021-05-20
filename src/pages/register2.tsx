81ac6f4d5472f057ab827e284d70fb1d6e08efa5
Frederico Neves <frednevesal@gmail.com>
2021-05-20 21:43:27 +0100

    register using formik

--- src/pages/register2.tsx
+++ src/pages/register2.tsx
@@ -1,98 +1,99 @@
-import { Formik, Form, useField } from 'Formik'
-import * as Yup from 'Yup';
+import {Formik, Field, Form, useField, FieldAttributes } from 'Formik'
+import {TextField, Button } from "@material-ui/core";
+import * as Yup from 'yup';
+import styles from './styles/register.module.scss'
+import  Head  from "next/head";
+import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
 
-const TextInput = ({ label, ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input>. We can use field meta to show an error
-    // message if the field is invalid and it has been touched (i.e. visited)
-    const [field, meta] = useField(props);
-    return (
-      <>
-        <label htmlFor={props.id || props.name}>{label}</label>
-        <input className="text-input" {...field} {...props} />
-        {meta.touched && meta.error ? (
-          <div className="error">{meta.error}</div>
-        ) : null}
-      </>
-    );
-  };
+const MyTextField: React.FC<FieldAttributes<{}>> = ({placeholder,...props}) =>{
+  const [field, meta] = useField<{}>(props);
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return(
+    <TextField color="secondary"  variant="outlined"
+    size="small" placeholder={placeholder} {...field} helperText={errorText} error={!!errorText} InputLabelProps={{
+      className: styles.input
+     
+    }}/>
+  )
+}
 
-export default function SignupForm() {
+const validationSchema = Yup.object({
+  username: Yup.string()
+      .matches(/^(\S+$)/, "Não pode conter espaços")
+      .min(5, "O nome da conta deve ter entre 5 a 15 caráteres.")
+      .max(15, "O nome da conta deve ter entre 5 a 15 caráteres.")
+      .required("Obrigatório"),
+  email: Yup.string()
+      .email("Por favor insira um email válido.")
+      .required("Obrigatório"),
+  password: Yup.string()
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+              "Deve conter no mínimo 8 caráteres com pelo menos 1 minúscula, 1 maiúscula e 1 dígito")
+      .required("Obrigatório"),
+      passwordConfirm: Yup.string()
+      .oneOf([Yup.ref('password'), null], "Passwords têm de ser iguais.")
+      .required("Obrigatório"),
+  firstName: Yup.string(),
+  lastName: Yup.string(),
+  phoneNumber: Yup.number().positive().integer("Não pode conter letras")
+      .test("len", "Deve ter exatamente 9 dígitos.", (val) => { if(val) return val.toString().length === 9; })
+      .typeError("Não pode conter letras.")
+});
 
-    return (
-        <Formik
-            initialValues = {{
-                title: '',
-                description: '',
-                date: '',
-                location: '',
-                number_of_people: ''
+export default function Register(){
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <NavBar/><h1>Inscreva-se</h1>
+      <div className={styles.register}>
+      
+      <Formik initialValues = {{
+                username: '',
+                email: '',
+                password: '',
+                passwordConfirm: '',
+                firstName: '',
+                lastName: '',
+                phoneNumber: ''
             }}
-            validationSchema = {Yup.object({
-                title: Yup.string()
-                    .min(10, "O título deve ter entre 10 a 50 caráteres.")
-                    .max(50, "O título deve ter entre 10 a 50 caráteres.")
-                    .required("Obrigatório"),
-                description: Yup.string()
-                    .min(100, "A descrição deve conter no minímo 100 caráteres.")
-                    .required("Obrigatório"),
-                date: Yup.string()
-                    .required("Obrigatório"),
-                location: Yup.string()
-                    .required("Obrigatório"),
-                number_of_people: Yup.number("Não pode conter letras.")
-                    .moreThan(0, "Uma atividade precisa de pelo menos uma pessoa.")
-                    .typeError("Não pode conter letras.")
-                    .required("Obrigatório"),
-            })}
-            onSubmit = // FUNCAO DO SUBMIT AQUI
-            //--------------------------
-            //--------------------------
-            //TODO
-            //--------------------------
-            //--------------------------
-                { values => {
-                alert(JSON.stringify(values, null, 2));
-            }
-            }
-        >
+            validationSchema = {validationSchema}
+            
+            //resetform
+            onSubmit={(data, {setSubmitting}) => {
+              setSubmitting(true);
+              
+              console.log('submit:',data);
+              setSubmitting(false);
+            }}>
 
-        <Form>
+        {({values,isSubmitting}) => (
+          <Form className={styles.form}  >
+            <MyTextField className={styles.input} id="outlined-basic" placeholder="username"name="username" type="input" as={TextField}/>
+            <MyTextField placeholder="email" name="email" type="input" as={TextField}/>
+            <MyTextField placeholder="passwrod" name="password" type="password" as={TextField}/>
+            <MyTextField placeholder="confirm password" name="passwordConfirm" type="password" as={TextField}/>
+            <MyTextField placeholder="first name" name="firstName" type="input" as={TextField}/>
+            <MyTextField placeholder="last name" name="lastName" type="input" as={TextField}/>
+            <MyTextField placeholder="phone number" name="phoneNumber" type="input" as={TextField}/>
+                      <div>
+                      <Button disabled={isSubmitting} type="submit">submit</Button>
+                      </div>
+          </Form>
 
-            <TextInput
-                label="Titulo"
-                name="title"
-                type="text"
-            />
 
-            <TextInput
-                label="Descrição"
-                name="description"
-                type="text"
-            />
 
-            <TextInput
-                label="Data"
-                name="date"
-                type="datetime-local"
-            />
+      )
 
-            <TextInput
-                label="Localização"
-                name="location"
-                type="text"
-            />
 
-            <TextInput
-                label="Pessoas necessárias"
-                name="number_of_people"
-                type="text"
-            />
+        }
+      </Formik>
+      </div>
+      <Footer/>
+    </div>
 
-            <p/>
-            <button type="submit">Submit</button>
+  );
 
-        </ Form>
-    </ Formik>
-    );   
 }
