4386d9d82ed8d5bef35da2a3d2db2328ace3693b
Frederico Neves <frednevesal@gmail.com>
2021-06-28 19:12:33 +0100

    usestate

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,16 @@
 import styles from "./styles/base.module.scss";
-import {useState} from 'react'
-import  Head  from "next/head";
+import { useState } from 'react'
+import Head from "next/head";
 import Footer from '../Components/Footer';
-import Header from  '../Components/Header';
-import  Link from 'next/link';
-import {AuthContext} from '../Context/AuthContext'
-import React, {useContext, useEffect} from 'react'
+import Header from '../Components/Header';
+import Link from 'next/link';
+import { AuthContext } from '../Context/AuthContext'
+import React, { useContext, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { api } from "../../services/api";
 import useSWR from 'swr'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import SessionOf from '../Components/SessionOf'
 import MapView from "../Components/Maps";
 import SearchIcon from '@material-ui/icons/Search';
@@ -21,15 +21,15 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 type Token = {
   username: string,
-   tokenID: string,
-    role: string,
-     creationData: number, 
-     expirationData: number
+  tokenID: string,
+  role: string,
+  creationData: number,
+  expirationData: number
 }
 
 type AtivitiesProps = {
   title: string,
-  description: string ,
+  description: string,
   date: string,
   location: string,
   totalParticipants: string,
@@ -40,57 +40,62 @@ type AtivitiesProps = {
 type listAtivitiesProps = AtivitiesProps[];
 
 //await api.post('activities/list',token)
-async function fetcher(path:string): Promise<listAtivitiesProps> {
+async function fetcher(path: string): Promise<listAtivitiesProps> {
 
-  const token: Token  = Cookies.getJSON('token')
-  return  await api.post(path,token).then(response => response.data);
-  
+  const token: Token = Cookies.getJSON('token')
+  return await api.post(path, token).then(response => response.data);
+
 }
 
 export default function Home() {
-  const{subAtivity,setSubAtivity, authenticated } = useContext(AuthContext);
+  const { subAtivity, setSubAtivity, authenticated } = useContext(AuthContext);
 
   const router = useRouter();
   useEffect(() => {
-    if(!authenticated){
+    if (!authenticated) {
       router.push('/login')
     }
-  },[])
+  }, [])
 
   const [state, setState] = React.useState({
     checkedA: true,
   });
-  
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
 
 
-  const { data, error} = useSWR('activities/list', fetcher);
-  
+  const { data, error } = useSWR('activities/list', fetcher);
+  const [val, setVal] = useState("");
+
+  const handleVal = (e) => {
+    setVal(e.target.value)
+    console.log(val)
+  }
 
   let props: listAtivitiesProps = data;
 
-  
-  if (error) {return <SessionOf/>}
+
+  if (error) { return <SessionOf /> }
   if (!data) return <div>loading...</div>
 
   return (
-    
+
     <div className={styles.Container} >
       <Head>
         <title>Home</title>
-        </Head>
-      
+      </Head>
+
       <div className={styles.Header} >
-        <Header/>
+        <Header />
       </div>
       <div className={styles.Feed}>
 
         <div className={styles.searchBar}>
           <button><SearchIcon fontSize="large" ></SearchIcon></button>
-          <input name="pesquisa" placeholder="Pesquisa"></input>
+          <input name="pesquisa" placeholder="Pesquisa" onChange={(e) => handleVal(e)}></input>
 
           <LocationOnIcon fontSize="large"></LocationOnIcon>
           <select className={styles.select} name="distrito" id="Portugal">
@@ -125,78 +130,78 @@ export default function Home() {
             <option value="Ilha das Flores">Ilha das Flores</option>
             <option value="Ilha do Corvo">Ilha do Corvo</option>
           </select>
-          
+
         </div>
-     
+
         <FormControlLabel
           control={
-            <Checkbox 
+            <Checkbox
               checked={state.checkedA}
               onChange={handleChange}
               name="checkedA"
-              color="default" 
+              color="default"
             />
           }
           label="Mostrar apenas as atividades das organizações que sigo"
         />
         <FormControlLabel
           control={
-            <Checkbox 
+            <Checkbox
               checked={state.checkedA}
               onChange={handleChange}
               name="checkedA"
-              color="default" 
+              color="default"
             />
           }
           label="Mostrar apenas atividades de organizações"
         />
         <FormControlLabel
           control={
-            <Checkbox 
+            <Checkbox
               checked={state.checkedA}
               onChange={handleChange}
               name="checkedA"
-              color="default" 
+              color="default"
             />
           }
           label="Mostrar apenas atividades de indivíduos"
         />
-        <br/>
+        <br />
 
         <div>
-          {props.map((ativ,index) =>{
+          {props.map((ativ, index) => {
             return (
               <div className={styles.activity} key={index}>
-                  <Link href="/{ativ.title}"> 
-                    {ativ.title}
-                  </Link>
-                  <p>
-                    {ativ.description}
-                  </p>
-                  <p>
-                    {ativ.date}
-                  </p>
-                  <p>
-                    {ativ.location}
-                  </p>
-                  <p>
-                    {ativ.totalParticipants}
-                  </p>
-                  <p>
-                    {ativ.activityOwner}
-                  </p>
-                  <p>
-                    {ativ.category}
-                  </p>
-            </div>
+                <Link href="/{ativ.title}">
+                  {ativ.title}
+                </Link>
+                <p>
+                  {ativ.description}
+                </p>
+                <p>
+                  {ativ.date}
+                </p>
+                <p>
+                  {ativ.location}
+                </p>
+                <p>
+                  {ativ.totalParticipants}
+                </p>
+                <p>
+                  {ativ.activityOwner}
+                </p>
+                <p>
+                  {ativ.category}
+                </p>
+              </div>
             )
           })}
         </div>
-        
+
       </div>
 
     </div>
-     
+
   )
 }
 
