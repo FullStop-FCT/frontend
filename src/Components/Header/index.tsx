bb9a66c5a8131739964f31d34343d2567bf182d9
Frederico Neves <frednevesal@gmail.com>
2021-06-29 23:05:54 +0100

    css editing

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -4,11 +4,16 @@ import styles from './styles.module.scss'
 import Link from 'next/link';
 import { AuthContext } from '../../Context/AuthContext'
 import React, { useContext } from 'react'
+import Cookies from 'js-cookie'
+import { CgProfile } from 'react-icons/cg'
+import { FaHandHoldingHeart } from 'react-icons/fa'
+import { IoCreateSharp, IoNotifications } from 'react-icons/io5'
 export default function NavBar() {
 
 
   const [state, setState] = useState(false)
   const { handleLogout } = useContext(AuthContext);
+  const Token = Cookies.get('user')
   const handleClick = () => {
     setState(!state)
   }
@@ -17,30 +22,33 @@ export default function NavBar() {
   return (
     <div className={styles.Container}>
       <div className={styles.NavbarItems}>
-        <nav >
-          <h1>Helpin'Hand</h1>
-          <div className={styles.Navbar}>
-            <ul>
-              {menu.map((item, index) => {
-                return (
-                  <li key={index}>
 
-                    <Link href={item.url} ><a className={item.cName}
+        <h1>Helpin'Hand</h1>
+        <div className={styles.Navbar}>
 
-                    >{item.title}</a></Link>
-
-                  </li>
-
-                )
-              })}
-
-            </ul>
-            <button onClick={() => handleLogout()}>Logout</button>
+          <div className={styles.topics}>
+            <Link href={`/${Token}`}><a className={styles.links} ><CgProfile /><a className={styles.linkname}> Perfil</a></a></Link>
           </div>
 
-        </nav>
+          <div className={styles.topics}>
+            <Link href={'/home'}><a className={styles.links}><FaHandHoldingHeart /><a className={styles.linkname}> Explorar</a></a></Link>
+          </div>
+
+          <div className={styles.topics}>
+            <Link href={'/createActivity'}><a className={styles.links}><IoCreateSharp /><a className={styles.linkname}> Criar Atividade</a></a></Link>
+          </div>
+
+          <div className={styles.topics}>
+            <Link href={`/${Token}`}><a className={styles.links}><IoNotifications /><a className={styles.linkname}> Notificações</a></a></Link>
+          </div>
+
+
+          <button onClick={() => handleLogout()}>Logout</button>
+        </div>
+
+
       </div>
-    </div>
+    </div >
 
   )
 }
--- src/pages/registeroption.tsx
+++ src/pages/registeroption.tsx
@@ -1,32 +1,32 @@
 import styles from './styles/registeroption.module.scss'
-import  Head  from "next/head";
+import Head from "next/head";
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
-export default function Login(){
+export default function Login() {
   //css por fazer
-  return(
+  return (
     <div >
-    <Head>
+      <Head>
         <title>Register</title>
       </Head>
-      <NavBar/>
+      <NavBar />
       <div className={styles.h1}> <h1>Escolha uma categoria</h1></div>
       <div className={styles.imagediv}>
-      
+
         <div className={styles.image1}>
-         <a href="/register"><img src="/voluntariado.png"></img></a> 
-         <h1>Voluntariado</h1>
+          <a href="/register"><img src="/voluntariado.png"></img></a>
+          <h1>Voluntariado</h1>
         </div>
         <div className={styles.image2}>
-        <a><img  src="/organizacao.png"></img></a> 
-        <h1>Organização</h1>
+          <a><img src="/organizacao.png"></img></a>
+          <h1>Organização</h1>
         </div>
-        
+
       </div>
-         
-     
-      
-      <Footer/>
+
+
+
+      <Footer />
     </div>
 
 
