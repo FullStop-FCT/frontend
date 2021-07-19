b474ec0475df974f2da660c71e0ca5acffec48a7
Frederico Neves <frednevesal@gmail.com>
2021-07-20 00:07:16 +0100

    update account activation

--- src/pages/activeacc/[token].tsx
+++ src/pages/activeacc/[token].tsx
@@ -19,27 +19,29 @@ export default function token(){
   const router = useRouter();
   let token = window.location.pathname.replace('/', '')
   let path_values: String[] = token.split("/");
- 
+  const[active,setActive] = useState<number>(null);
   let jwt = path_values[1];
 
   //let {data, error} = useSWR(['users/confirmSignup', jwt], fetcher);
 
-  useEffect(  async () => {
-    let decodetoken: Token = jwt_decode(token);
-    let config = {
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Content-Type': 'application/json'
+  useEffect( () => {
+    const funct = async () => {
+      let decodetoken: Token = jwt_decode(token);
+      let config = {
+        headers: {
+          'Authorization': 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        }
       }
-    }
-    console.log(token)
-     await api.post('users/confirmSignup',decodetoken.iss,config).then(response => response.status)
-
-
-  },[])
+      console.log(token)
+       await api.post('users/confirmSignup',decodetoken.iss,config).then(response => setActive(response.status)).catch(error => setActive(error.response.status))
   
-
-console.log(error)
-if (!data) return <Loading />    
-if (error || data === 403) return <div>Nao foi possivel ativar a conta</div>
+    }
+    funct()
+   
+  },[])
+  console.log(active)
+  if(active === null) return <Loading />
+  if(active === 200)  return <div>activado</div>
+  else return <div>error</div>
   }
