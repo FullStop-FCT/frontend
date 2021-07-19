454a28509bc09f60c64aa68e5b3b170aea3f1f94
vsc9 <62213568+vsc9@users.noreply.github.com>
2021-07-19 17:09:09 +0100

    stripe

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,7 +11,7 @@ import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import Link from 'next/link';
 import { Token } from '../types';
-import jwt_decode from "jwt-decode"
+import jwt_decode from "jwt-decode";
 
 //TODO
 
