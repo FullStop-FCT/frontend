ef7291f502b3fa1c99b03466710bce7de89db819
Frederico Neves <frednevesal@gmail.com>
2021-07-25 23:11:12 +0100

    favicon update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
 
-
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
           <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
 
 
